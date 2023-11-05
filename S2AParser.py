0b9ca442326ebfa32e59f4a88c728b852365ff12
Jaber Abbaszadeh <37061699+jabsz@users.noreply.github.com>
2023-11-05 17:36:36 +1300

    Update S2AParser.py

--- S2AParser.py
+++ S2AParser.py
@@ -1,143 +1,141 @@
 #!/usr/bin/env python3
 
+###############################################################
+# Author: Jaber Abbaszadeh 
+# Project: Annual report ERI University of Waikato 
+# This script takes semantic scholar author IDs and
+#   generates a list of publications for ERI annual report
+###############################################################
+
 import argparse
 import pandas as pd
-import json
 import requests
-import logging
-from semanticscholar import SemanticScholar
 from datetime import datetime
 
-# Parts of this script was inspired from PHP CrossRef Client
-
 startTime = datetime.now()
-# prerequisits: 1-semanticscholar,
-this_year=startTime.year
-last_year=this_year-1
+this_year = startTime.year
+last_year = this_year - 1
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Generates a table containing a list of authors and pulications",
+    parser = argparse.ArgumentParser(description="Generates a table containing a list of authors and publications",
                                      formatter_class=argparse.HelpFormatter)
     parser.add_argument("-i", "--input", type=argparse.FileType(mode='r'), help="Input Path")
     parser.add_argument("-o", "--output", type=argparse.FileType('wb', 0), help="Output Path",
                         default='output.csv')
-    parser.add_argument("-sy", "--yearS", type=int, help="Specify start year",
+    parser.add_argument("--yearS", type=int, help="Specify start year",
                         metavar='Start Year, default = Last Year',
                         default=last_year)
-    parser.add_argument("-ey", "--yearE", type=int, help="Specify end year", metavar='End Year, default = This Year',
+    parser.add_argument("--yearE", type=int, help="Specify end year", metavar='End Year, default = This Year',
                         default=this_year)
-    parser.add_argument("-t", "--timeout", type=int, help="Specify Timeout", default=30000)
+    parser.add_argument("-v", "--verbose", action="store_true", help="Turn on verbose output")
     args = parser.parse_args()
 
-print("Please wait, fetching the data takes a while...")
+    if args.input is None:
+        parser.print_help()
+        exit(1)
 
-
-class CrossRefClient(object):
-    def __init__(self, accept='text/x-bibliography; style=apa', timeout=args.timeout):
-        """
-        # Defaults to APA biblio style
-
-        # Usage:
-        s = CrossRefClient()
-        """
-        self.headers = {'accept': accept}
-        self.timeout = timeout
-
-    def query(self, doi, q={}):
-        if doi.startswith("http://"):
-            url = doi
-        else:
-            url = "http://dx.doi.org/" + doi
-
-        r = requests.get(url, headers=self.headers)
-        return r
-
-    def doi2apa(self, doi):
-        self.headers['accept'] = 'text/x-bibliography; style=apa'
-        return self.query(doi).text
-
-    def doi2json(self, doi):
-        self.headers['accept'] = 'application/vnd.citationstyles.csl+json'
-        return self.query(doi).json()
-
+print("Please wait, fetching the data takes a while... \nfor activating interactive parsing mode use -v option")
 
 author_list = pd.read_csv(args.input)
-author_id = author_list['author_id']
+author_ids = author_list['author_id']
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, filename="report.csv", filemode="w+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+def format_author_name(name):
+    # Split the author name into words
+    words = name.split()
+    # Extract the last name
+    last_name = words[-1]
+    # Extract the initials of other names
+    initials = " ".join(word[0].upper() + '.' for word in words[:-1])
+    # Format the name as "Last Name, Initials"
+    formatted_name = f"{last_name}, {initials}"
+    return formatted_name
 
-# make an empty list for later, and loop through author ids and find their papers (after 2020)
+# Define the base URLs
+base_url_author = "https://api.semanticscholar.org/graph/v1/author/{}"
+base_url_paper = "https://api.semanticscholar.org/graph/v1/author/{}/papers?fields=url,title,venue,year,authors,externalIds"
+
+# Define the author IDs (you can put multiple author IDs in a list)
+author_id_to_name = dict(zip(author_list['author_id'], author_list['Name']))
+
 table = []
-for id in author_id:
-    sch = SemanticScholar(timeout=1800000)
-    author = sch.author(id)
-    author_paper = (author['papers'])
-    author_name = (author['name'])
-    df_papers = pd.DataFrame(author_paper)
-    year_paper = df_papers[((df_papers['year'] >= args.yearS) & (df_papers['year'] <= args.yearE))]
+for author_id in author_ids:
+    # Create the URLs for the author and papers
+    url_member = base_url_author.format(author_id)
+    url_papers = base_url_paper.format(author_id)
 
-    paper_id = (year_paper['paperId'])
+    # Fetch data for the author and papers
+    response_member = requests.get(url_member)
+    response_paper = requests.get(url_papers)
 
-    # first write authors name and then loop through their papers
-    # then wirte table which is a extracted list of authour name and publication details from dictionarey.
+    if response_member.status_code == 200 and response_paper.status_code == 200:
+        json_member = response_member.json()
+        json_paper = response_paper.json()
 
-    for pid in paper_id:
-        paper_dic = sch.paper(pid)
-        keys = ['title', 'doi', 'year', 'venue']
-        # authors_list = [d['name'] for d in mt.collapse(paper_dic.get("authors"), base_type=dict)]
-        data = [paper_dic.get(key) for key in keys]
-        list_dic = [author_name, data[0], data[1], data[2], data[3]]
-        # out = [author_name, [paper_dic.get(key) for key in keys],authors]
-        table.append(list_dic)
-        df_table = pd.DataFrame(table)
+        # Accessing author information
+        member = json_member['name']
+        member_id = json_member['authorId']
 
-# get the doi number and generate apa format for bibliography
-cite = []
-for doi in df_table[2]:
-    try:
-        session = CrossRefClient()
-        out = session.doi2json(doi)
+        # Access paper data for the author
+        data = json_paper['data']
 
+        publications_added = False  # Flag to check if any publications were added for this author
 
-        def names(ref):
-            name = []
-            for _item in ref['author']:
-                given_in = _item['given'].split(' ')
-                given = ''.join([_name[0] + '.' for _name in given_in])
-                name.append(_item['family'] + ', ' + given + ', ')
-            return ''.join(name[0:-1]) + 'and ' + name[-1][0:-2]
+        for paper in data:
+            member = author_id_to_name.get(author_id, "Unknown Author")  # Get the author_name from the dictionary
+            author_name= author_list['Name']
+            title = paper['title']
+            authors = paper['authors']
+            author_names = [author['name'] for author in authors]
+            journal = paper['venue']
+            
+            #Some records of Semantic scholar have null year, this makes sure that they dont interrupt the script
+            try:
+                year = int(paper['year'])
+            except (ValueError, TypeError):
+                # Skip this paper if the year is not a valid integer
+                print(f"Skipping paper with invalid year, Author", member, "title:" ,title, )
+                continue
 
+            if args.verbose:
+                print("getting data for  ", member, "with author ID ", member_id, "paper title:", title, "year", year)
 
-        authors = names(out)
-        year = str(out['created']['date-parts'][0][0])
-        title = out['title']
-        journal_short = str(out['container-title'])
-        # volume = str(out['volume'])
-        # pages = str(out['article-number'])
-        DOI = out['DOI']
-        bib = [authors + ' ' + year + '. ' + title + '. ' + journal_short + '. ' + 'doi:/' + DOI]
-        cite.append(bib[0])
-        try_index = (len(cite) + 2)
-    except (AttributeError, KeyError, json.decoder.JSONDecodeError, NameError) as e:
-        # print('Check row', try_index, ' No DOI number was detected for this publication. Edit manually')
-        logging.info(', Row %s has no doi number. Edit manually', try_index)
-        cite.append('There is no DOI number for this publication. Edit manualy')
-        # logging.basicConfig(filename="report.txt", level=logging.DEBUG)
-        # logger.error('Check row', try_index ,' No DOI number was detected for this publication. Edit manually')
+            # Check if DOI is available
+            if 'externalIds' in paper and 'DOI' in paper['externalIds']:
+                doi = paper['externalIds']['DOI']
+            else:
+                doi = " "  # Replace with a space if DOI is not available
+            # Filter papers by year (between 2021 and 2022)
 
-# Add bibliography to the dataframe and write as csv file:
-df_table['Bibliography'] = cite
+            if (last_year <= year <= this_year) or (args.yearS <= year <= args.yearE):
+                title = title.replace('\n', ' ').strip()
+                # Create a list of formatted author names
+                formatted_author_names = [format_author_name(author) for author in author_names]
+                # Join the formatted author names into a single string
+                author_names_str = ", ".join(formatted_author_names)
+                # Print the information for each paper
+                biblio = [author_names_str, year, title, journal, doi]
+                biblio_frmt = [str(item).strip("[]''") for item in biblio]
+                biblio_str = ". ".join(biblio_frmt)
+
+                table.append((member, title, doi, year, journal, biblio_str))
+                publications_added = True  # Publications were added
+
+        if not publications_added:
+            # If no publications were added for this author, add a row with "No publication"
+            table.append((member, "No publication", "No publication", "No publication", "No publication", "No publication"))
+
+    else:
+        print(f"Failed to fetch data for author {author_id}")
+
 titles = ['Name', 'Title', 'DOI', 'Year', 'Journal', 'Bibliography']
+# df_table.to_csv(args.output, header=titles, sep=',', index=None)
+df_table = pd.DataFrame(table)
 df_table.to_csv(args.output, header=titles, sep=',', index=None, encoding='utf-8-sig')
-# logger.info("this is info level")
-no_doi = len(df_table[df_table[2].isnull()])
 
-print(
-    "Caution: This list may include articles that don't belong to the author due to S2A errors,"
-    " It is recommended to check manually.")
-print(no_doi,
-      " records lacked doi number and the bibliography should be edited manually. Check report file for more... ")
-print(try_index, ' records were extracted for this list of authors')
-print('it took', datetime.now() - startTime, 'to fetch requested publications')
+
+print("\n Done... \n The list of ERI members and their publications for requested years is written as provided output table name or output.csv (default). \n Thanks  \n")
+
+# Print the names of authors with no publications
+no_publication_authors = [row[0] for row in table if row[1] == "No publication"]
+print("Authors with no publications in the specified years:", no_publication_authors)
+
